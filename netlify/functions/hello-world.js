a3882fe669aa1d6ec3179a30d1e2ff5c020f35bb
zhs <austin_zhs@163.com>
2024-12-19 14:49:28 +0800

    add a form

--- netlify/functions/hello-world.js
+++ netlify/functions/hello-world.js
@@ -1,8 +1,15 @@
 exports.handler = async (req) => {
-    console.log(req)
-    return {
-      statusCode: 200,
-      body: "hello world!"
-    };
+  let clientIP = req.ip;
+
+  // 检查 X-Forwarded-For 头
+  if (req.headers['x-forwarded-for']) {
+    const forwardedIps = req.headers['x-forwarded-for'].split(',');
+    clientIP = forwardedIps[0].trim(); // 使用最左边的 IP 地址
+  } else if (req.headers['x-real-ip']) {
+    clientIP = req.headers['x-real-ip'];
+  }
+  return {
+    statusCode: 200,
+    body: clientIP
   };
-  
+};
